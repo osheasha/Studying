e3305bda45afbb40e96f6f2d42efe3c9b051bfeb
Omar Sheasha <omar.sheasha@gmail.com>
2018-09-09 20:50:18 +0200

    Added new line

--- src/Assignment1_1.java
+++ src/Assignment1_1.java
@@ -2,7 +2,11 @@
 public class Assignment1_1 {
 	public static int run(int number) {
 		number = number + 1;
-		return number;
+		
 	}
 }
+
+git status : check status of code, if changes happened or not
+git diff: show the difference between old and new code
+
 
